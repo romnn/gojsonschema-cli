fcf5e28c2cc0a0a7e97f9c27aceabda56027c9e6
romnn <roman.dahm@gmx.de>
2024-10-20 03:33:50 +0200

    initial commit

---
+++ cmd/jsonschema/main.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/xeipuuv/gojsonschema"
+	"gopkg.in/yaml.v3"
+	k8syaml "sigs.k8s.io/yaml"
+
+	_ "github.com/joho/godotenv/autoload"
+	prettyconsole "github.com/thessem/zap-prettyconsole"
+	"github.com/urfave/cli/v3"
+	"go.uber.org/zap"
+	"golang.org/x/term"
+	errors "golang.org/x/xerrors"
+)
+
+var (
+	schemaLocationFlag = cli.StringFlag{
+		Name:    "schema",
+		Aliases: []string{"s"},
+		Usage:   "path to schema",
+	}
+
+	valuesLocationFlag = cli.StringFlag{
+		Name:    "values",
+		Aliases: []string{"v"},
+		Usage:   "path to values",
+	}
+)
+
+const (
+	defaultLogLevel = zap.FatalLevel
+)
+
+type JSONSchema struct {
+	Schema string `json:"$schema,omitempty"`
+}
+
+func loadFile(path string) ([]byte, error) {
+	return os.ReadFile(path)
+}
+
+func loadURL(u *url.URL) ([]byte, error) {
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return []byte{}, err
+	}
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return data, nil
+}
+
+func resolve(location string) ([]byte, error) {
+	if u, err := url.Parse(location); err == nil {
+		switch u.Scheme {
+		case "file":
+			// resolve file
+			return loadFile(u.Host)
+		case "http", "https":
+			// resolve url
+			return loadURL(u)
+		default:
+			// ignore
+		}
+	}
+	// resolve file
+	return loadFile(location)
+}
+
+func validate(_ context.Context, cmd *cli.Command, logger *zap.Logger) error {
+	schemaLocation := cmd.String(schemaLocationFlag.Name)
+	if schemaLocation == "" {
+		return fmt.Errorf("missing schema")
+	}
+	schemaJSON, err := resolve(schemaLocation)
+	if err != nil {
+		return err
+	}
+
+	swap := false
+	valuesLocation := cmd.String(valuesLocationFlag.Name)
+	if valuesLocation == "" {
+		var schema JSONSchema
+		if err := json.Unmarshal(schemaJSON, &schema); err != nil {
+			return err
+		}
+		valuesLocation = schema.Schema
+		swap = true
+	}
+
+	if valuesLocation == "" {
+		return fmt.Errorf("missing values")
+	}
+
+	valuesJSON, err := resolve(valuesLocation)
+	if err != nil {
+		return err
+	}
+
+	if swap {
+		valuesLocation, schemaLocation = schemaLocation, valuesLocation
+		valuesJSON, schemaJSON = schemaJSON, valuesJSON
+	}
+
+	// fmt.Printf("##### schema:\n%s\n", schemaJSON)
+	// fmt.Printf("##### values:\n%s\n", valuesJSON)
+
+	// make sure schema is valid JSON
+	var schema any
+	if err := json.Unmarshal(schemaJSON, &schema); err != nil {
+		// try to parse from YAML
+		if err := yaml.Unmarshal(schemaJSON, &schema); err == nil {
+			// convert to JSON
+			var err error
+			schemaJSON, err = k8syaml.YAMLToJSON(schemaJSON)
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+
+	// make sure values is valid JSON
+	var values any
+	if err := json.Unmarshal(valuesJSON, &values); err != nil {
+		// try to parse from YAML
+		if err := yaml.Unmarshal(valuesJSON, &values); err == nil {
+			// convert to JSON
+			var err error
+			valuesJSON, err = k8syaml.YAMLToJSON(valuesJSON)
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+
+	if bytes.Equal(valuesJSON, []byte("null")) {
+		valuesJSON = []byte("{}")
+	}
+
+	schemaLoader := gojsonschema.NewBytesLoader(schemaJSON)
+	valuesLoader := gojsonschema.NewBytesLoader(valuesJSON)
+
+	result, err := gojsonschema.Validate(schemaLoader, valuesLoader)
+	if err != nil {
+		return err
+	}
+
+	if !result.Valid() {
+		var sb strings.Builder
+		for _, desc := range result.Errors() {
+			logger.Error(desc.Description(), zap.String("field", desc.Field()))
+			sb.WriteString(fmt.Sprintf("- %s\n", desc))
+		}
+		return errors.New(sb.String())
+	}
+	return nil
+}
+
+func main() {
+	var logger *zap.Logger
+	if term.IsTerminal(int(os.Stdout.Fd())) {
+		logger = prettyconsole.NewLogger(defaultLogLevel)
+	} else {
+		logger, _ = zap.NewProduction()
+	}
+	defer logger.Sync()
+
+	app := cli.Command{
+		Name:        "jsonschema",
+		Description: "jsonschema",
+		Usage:       "jsonschema",
+		Flags:       []cli.Flag{},
+		Commands: []*cli.Command{
+			{
+				Name:  "validate",
+				Usage: "validate",
+				Flags: []cli.Flag{
+					&schemaLocationFlag,
+					&valuesLocationFlag,
+				},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					return validate(ctx, cmd, logger)
+				},
+			},
+		},
+	}
+
+	err := app.Run(context.Background(), os.Args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/romnn/gojsonschema-cli
+
+go 1.23.1
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/thessem/zap-prettyconsole v0.5.2
+	github.com/urfave/cli/v3 v3.0.0-alpha9.1
+	github.com/xeipuuv/gojsonschema v1.2.0
+	go.uber.org/zap v1.27.0
+	golang.org/x/term v0.25.0
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da
+	gopkg.in/yaml.v3 v3.0.1
+	sigs.k8s.io/yaml v1.4.0
+)
+
+require (
+	github.com/Code-Hex/dd v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
